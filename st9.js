d7884e07d30fe2329fc78ae92cb63ca726aa067c
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-03 10:56:10 +0700

    update st9.js

--- st9.js
+++ st9.js
@@ -1,7 +1,30 @@
-let a = 5;
-let b = 10;
-let c = 15;
+// Задача: попрактиковаться в сравнениях разных типов данных
 
-let sample = a + b;
+let number1 = 5;
+let number2 = 10;
+let number3 = 15;
 
-console.log(sample == c);
+let sample = number1 + number2;
+
+let string = 'JavaScript';
+let string2 = 'GitHub';
+let string3 = 'Java'+'Script';
+
+let boolean = true;
+let boolean2 = false;
+
+// number
+console.log(sample === number3);
+console.log(sample === number2);
+console.log(sample !== number1);
+
+// string
+console.log(string === string2);
+console.log(string === string3);
+console.log(string > string2);
+console.log(number1 >= number3);
+console.log(number3 <= sample);
+
+// boolean
+console.log(boolean === boolean2);
+console.log(boolean !== boolean2);
