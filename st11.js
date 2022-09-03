17008d1fac82dbc2d06ae2dd3b728dd7202c3ab4
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-03 11:04:43 +0700

    calc update

--- st11.js
+++ st11.js
@@ -13,18 +13,3 @@ function calc(operator, a, b) {
 console.log (calc('add', 5, 5));
 console.log (calc('multi', 5, 5));
 console.log (calc('subtract', 10, 5));
-
-
-
-function something(name, age) {
-    newName = name + ', ' + age + ' годика. ' + 'Скоро едем в Питер!';
-    console.log(newName);
-
-    if(name == 'Женечка') {
-        let newName2 = name + ', ' + age + ' годиков. ' + 'Скоро едем в Питер!';
-        console.log(newName2);
-    }
-}
-
-something('Denis', 33);
-something('Женечка', 26);
