82ff639b463a15abe23f714f80b6b71f32c0d8db
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-04 16:02:38 +0700

    Add LearnJS Task

--- st5.js
+++ st5.js
@@ -1,14 +1,14 @@
-// Задача создать различные переменные и вывести их в консоль 
+// Задача создать различные переменные и вывести их в консоль
 
 let a = 10;
 let b = 10;
 let c = a + b;
 
 let word;
-word = 'Some text';
+word = "Some text";
 
 let any;
-any = a==b;
+any = a == b;
 
 console.log(c);
 console.log(word);
@@ -16,3 +16,26 @@ console.log(any);
 console.log(typeof a);
 console.log(typeof word);
 console.log(typeof any);
+
+// Задачи с LearnJavaScript
+
+// Task#1
+/* Объявите две переменные: admin и name.
+Запишите строку "Джон" в переменную name.
+Скопируйте значение из переменной name в admin.
+Выведите на экран значение admin, используя функцию alert (должна показать «Джон»). */
+
+let admin;
+let name1;
+name1 = "Джон";
+admin = name1;
+
+alert(admin);
+console.log(name1);
+console.log(admin);
+
+// Task#2
+/* Создайте переменную для названия нашей планеты. Как бы вы её назвали?
+Создайте переменную для хранения имени текущего посетителя сайта. Как бы вы назвали такую переменную? */
+let ourPlanetName = "Earth";
+let currentUserName = "Denis";
