03e87d55873277cf0c97a969624d0a67d885e3f0
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-04 20:42:32 +0700

    Add example type

--- st6.js
+++ st6.js
@@ -18,8 +18,7 @@ let sample8 = c + d;
 let sample9 = e + f;
 let sample10 = a + b + c;
 let sample11 = ++b;
-let sample12 = a+= ++b;
-
+let sample12 = (a += ++b);
 
 console.log(sample1);
 console.log(sample2);
@@ -33,3 +32,45 @@ console.log(sample9);
 console.log(sample10);
 console.log(sample11);
 console.log(sample12);
+
+// Пример с курсов Дениса Мещерякова
+let value;
+
+// Number to string
+value = String(10);
+value = String(10 + 40);
+value = (40).toString();
+
+// Boolean to string
+value = String(false);
+// Array to string
+value = String([1, 2, 3]);
+// Object to string
+value = String({ name: "Denis" });
+
+value = 30 + "" + undefined;
+value = 30 * "5";
+value = false + undefined;
+
+// String to number
+value = Number("23");
+value = Number(false);
+value = Number(null);
+value = Number("false");
+value = Number([1, 2, 3]);
+
+value = parseInt("200px");
+value = parseFloat("200.212px");
+
+// Boolean
+value = Boolean("hello");
+value = Boolean("");
+value = Boolean(-100);
+value = Boolean(0);
+value = Boolean(undefined);
+value = Boolean(null);
+value = Boolean({});
+value = Boolean([]);
+
+console.log(value);
+console.log(typeof value);
