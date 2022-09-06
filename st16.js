d75284067852f9419c6c1eac63b5df4e6aad6a66
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-06 16:00:40 +0700

    Code correction

--- st16.js
+++ st16.js
@@ -1,33 +1,43 @@
-// function calc(operator, a, b) {
-//     const operations = {
-//         add: '+',
-//         multi: '*',
-//         subtract: '-',
-//     }
+const operations = {
+  add: '+',
+  multi: '*',
+  subtract: '-',
+};
 
-//     switch (operator) {
-//         case 'add':
-//             return a + b;
-//         case 'multi':
-//             return a * b;
-//         case 'subtract':
-//             return a - b;
-//         default:
-//             console.log('Ошибка');
-//     }
-// }
+function calc(operator, a, b) {
+  if (
+    operator !== operations.add &&
+    operator !== operations.multi &&
+    operator !== operations.subtract
+  ) {
+    return console.log('Неверный оператор!');
+  }
+  switch (operator) {
+    case operations['add']:
+      return a + b;
+    case operations['multi']:
+      return a * b;
+    case operations['subtract']:
+      return a - b;
+    default:
+      console.log('Ошибка');
+  }
+}
 
-// console.log(calc('add', 5, 5));
-// console.log(calc('multi', 5, 5));
-// console.log(calc('subtract', 5, 5));
+console.log(calc('+', 10, 5));
+console.log(calc('*', 15, 10));
+console.log(calc('-', 25, 5));
 
+//
+//
+//
 let user = {};
 user.name = 'John';
 user.surname = 'Smith';
 user.name = 'Pete';
 // delete user.name;
 
-console.log(typeof(user));
+console.log(typeof user);
 console.log(user.name);
 console.log(user.surname);
 console.log(user.name);
