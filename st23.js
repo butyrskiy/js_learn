0a6b2f624f2fcce47741e31fdd9fd10538af0df4
Denis Butyrskiy <d.butyrskiy@gmail.com>
2022-09-09 09:34:56 +0700

    update file

--- st23.js
+++ st23.js
@@ -1,67 +1,76 @@
 function showVerticalMessage(message) {
-let firstSym = "S";
+  let firstSym = 'S';
 
-function str() {
-    for (let char of message.slice(0,7)) {
-        console.log(char);
+  function str() {
+    for (let char of message.slice(0, 7)) {
+      console.log(char);
     }
+  }
+
+  if (message[0] == 's') {
+    message = firstSym + message.slice(1);
+    str(message);
+    return;
+  }
+  if (typeof message == 'string') {
+    str(message);
+    return;
+  } else {
+    console.log('Введена не строка');
+  }
 }
 
-    if(message[0] == "s") {
-        message = firstSym + message.slice(1);
-        str(message);
-        return;
-    } if(typeof(message) == "string") {
-        str(message);
-        return;
-    } else {
-        console.log("Введена не строка");
-    }
-}
+showVerticalMessage('strada');
+showVerticalMessage('Приключения');
 
-showVerticalMessage("strada");
-showVerticalMessage("Приключения");
-
-
-
-/* // Задача с LearnJavaScript №1
+//
+//
+console.log('***');
+console.log('Задача с LearnJavaScript №1');
+console.log('***');
 
 function ucFirst(str) {
-    if(typeof(str) === "string") {
-        console.log(str[0].toUpperCase() + str.slice(1));
-    } else console.log("Проверка не пройдена");
+  if (typeof str === 'string') {
+    console.log(str[0].toUpperCase() + str.slice(1));
+  } else console.log('Проверка не пройдена');
 }
 
-ucFirst("denis");
+ucFirst('denis');
 
-
-// Задача с LearnJavaScript №2
+//
+//
+console.log('***');
+console.log('Задача с LearnJavaScript №2');
+console.log('***');
 
 function checkSpam(str) {
-let lowerStr = str.toLowerCase();
-    
-    if(typeof(lowerStr) != "string") {
-        return;    
-    } else if (lowerStr.includes("viagra") || lowerStr.includes("xxx")) {
-        console.log("True");
-    } else console.log("false");
+  let lowerStr = str.toLowerCase();
+
+  if (typeof lowerStr != 'string') {
+    return;
+  } else if (lowerStr.includes('viagra') || lowerStr.includes('xxx')) {
+    console.log('True');
+  } else console.log('false');
 }
 
-checkSpam("I buy Viagra to the XXX Shop");
+checkSpam('I buy Viagra to the XXX Shop');
 checkSpam('buy ViAgRA now');
 checkSpam('free xxxxx');
-checkSpam("innocent rabbit");
+checkSpam('innocent rabbit');
 
-
-// Задача с LearnJavaScript №3
+//
+//
+console.log('***');
+console.log('Задача с LearnJavaScript №3');
+console.log('***');
 
 function truncate(str, maxlength) {
-    if(str.length > maxlength) { 
-        console.log(str.slice(0,maxlength - 1) + "...");
-    } else {
-        console.log(str);
-    }
+  if (str.length > maxlength) {
+    console.log(str.slice(0, maxlength - 1) + '...');
+  } else {
+    console.log(str);
+  }
 }
 
-truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
-truncate("Всем привет!", 20); */
+truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
+truncate('Всем привет!', 20);
