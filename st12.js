f7bb11e508e593ddf631b2146e7b3e15953d4d45
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-01-06 00:28:22 +0300

    modified st12.js

--- st12.js
+++ st12.js
@@ -1,3 +1,7 @@
+console.log('***');
+console.log('Example #1');
+console.log('***');
+
 function calc(operator, a, b) {
   switch (operator) {
     case 'add':
@@ -14,8 +18,11 @@ function calc(operator, a, b) {
 console.log(calc('add', 5, 5));
 console.log(calc('multi', 5, 5));
 console.log(calc('subtract', 5, 5));
-
-// Example #2
+//
+//
+console.log('***');
+console.log('Example #2');
+console.log('***');
 
 function color(color) {
   switch (color) {
@@ -37,3 +44,78 @@ color('Red');
 color('White');
 color('Green');
 color('Black');
+//
+//
+//
+console.log('***');
+console.log('Example #3');
+console.log('***');
+
+/* Напишите if..else, соответствующий следующему switch: */
+
+/* let browser = '';
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+} */
+
+
+let browser = 'Opera';
+
+if(browser === 'Edge') {
+  alert("You've got the Edge!");
+} else if(browser === 'Chrome' 
+|| browser === 'Firefox' 
+|| browser === 'Safari' 
+|| browser === 'Opera') {
+  alert('Okay we support these browsers too');
+} else {
+  alert('We hope that this page looks ok!');
+}
+//
+//
+console.log('***');
+console.log('Example #4');
+console.log('***');
+/* Перепишите код с использованием одной конструкции switch: */
+
+/* const number = +prompt('Введите число между 0 и 3', '');
+
+if (number === 0) {
+  alert('Вы ввели число 0');
+}
+
+if (number === 1) {
+  alert('Вы ввели число 1');
+}
+
+if (number === 2 || number === 3) {
+  alert('Вы ввели число 2, а может и 3');
+} */
+//
+//
+let number;
+
+switch(number) {
+  case 0:
+    alert('Вы ввели число 0');
+    break;
+  case 1:
+    alert('Вы ввели число 1');
+    break;
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3');
+}
