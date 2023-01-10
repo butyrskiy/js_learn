475487f4f05b49ce640ffb28a2fb13fe6b014118
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-01-10 15:28:53 +0300

    modified st23.js

--- st23.js
+++ st23.js
@@ -1,27 +1,38 @@
-function showVerticalMessage(message) {
-  let firstSym = 'S';
+/* Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль. Чтобы получилось вот так:
 
-  function str() {
-    for (let char of message.slice(0, 7)) {
+showVerticalMessage('strada')
+S
+t
+r
+a
+d
+a
+
+Если строка начинается с буквы "s" - нужно вывести эту строку с первой заглавной буквой
+Если строка больше 7 символов - вывести только первые 7 */
+
+function showVerticalMessage(str) {
+  if(typeof(str) != 'string') {
+    return console.error('It\'s work just to string');
+  }
+
+  let newStr = str.slice(0, 7);
+  
+  if(newStr[0] === 's') {
+    let newStr2 = 'S' + str.slice(1);
+    for(let char of newStr2) {
       console.log(char);
     }
-  }
-
-  if (message[0] == 's') {
-    message = firstSym + message.slice(1);
-    str(message);
-    return;
-  }
-  if (typeof message == 'string') {
-    str(message);
-    return;
   } else {
-    console.log('Введена не строка');
-  }
+    for(let char of newStr) {
+      console.log(char);
+    } 
+  } 
 }
 
 showVerticalMessage('strada');
-showVerticalMessage('Приключения');
+showVerticalMessage('window');
+showVerticalMessage('application');
 
 //
 //
@@ -29,13 +40,19 @@ console.log('***');
 console.log('Задача с LearnJavaScript №1');
 console.log('***');
 
-function ucFirst(str) {
-  if (typeof str === 'string') {
-    console.log(str[0].toUpperCase() + str.slice(1));
-  } else console.log('Проверка не пройдена');
-}
+// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
 
-ucFirst('denis');
+ucFirst("вася") == "Вася";
+
+function ucFirst(str) {
+  if(!str) {
+    return console.log('Пустая строка');
+  }
+
+  let newStr = str.at(0).toUpperCase() + str.slice(1);
+  console.log(newStr);
+}
+ucFirst('evgenia');
 
 //
 //
@@ -43,13 +60,21 @@ console.log('***');
 console.log('Задача с LearnJavaScript №2');
 console.log('***');
 
+/* Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false */
+
 function checkSpam(str) {
   let lowerStr = str.toLowerCase();
 
   if (typeof lowerStr != 'string') {
     return;
   } else if (lowerStr.includes('viagra') || lowerStr.includes('xxx')) {
-    console.log('True');
+    console.log('true');
   } else console.log('false');
 }
 
@@ -64,13 +89,23 @@ console.log('***');
 console.log('Задача с LearnJavaScript №3');
 console.log('***');
 
+/* Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+
+Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка.
+
+Например:
+
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+
+truncate("Всем привет!", 20) = "Всем привет!" */
+
 function truncate(str, maxlength) {
-  if (str.length > maxlength) {
-    console.log(str.slice(0, maxlength - 1) + '...');
-  } else {
-    console.log(str);
-  }
+  let newStr;
+  if(str.length > maxlength) {
+    newStr = str.slice(0, maxlength - 1)+'...';
+    return newStr;
+  } else return str;
 }
 
-truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
-truncate('Всем привет!', 20);
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+console.log(truncate('Всем привет!', 20));
