22cac41a4011a66a6eed9fb93df4212951a511ab
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-01-31 18:14:13 +0300

    strada.task#16. add st7_16

---
+++ js/st7_16.js
@@ -0,0 +1,25 @@
+/* const telegramContacts = {
+  Wife: 5053770520,
+  'Father': 9050803377,
+  'Mother': 9050801133,
+  'Brother': 9050805599,
+  'Friend 0': 9132338866,
+  Barnaul: 666666,
+}
+
+telegramContacts.Friend = 9134552244;
+telegramContacts['Friend 1'] = 9137666600;
+telegramContacts.Friend = 'not a number';
+
+delete telegramContacts['Friend 0'];
+delete telegramContacts.Barnaul;
+
+console.log(telegramContacts); */
+
+
+const obj = {}
+const key = {}
+obj[key] = 'value for the object key'
+
+console.log(obj)
+// { '[object Object]': 'value for the object key' }
