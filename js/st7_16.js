19a4d6c70306b9251128dae06621a815154e2685
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-02-01 16:31:14 +0300

    add object "list" and add cicle "for in"

--- js/st7_16.js
+++ js/st7_16.js
@@ -1,31 +1,40 @@
 const telegramContacts = {
-  Wife: 5053770520,
-  'Father': 9050803377,
-  'Mother': 9050801133,
-  'Brother': 9050805599,
-  'Friend 0': 9132338866,
-  Barnaul: 666666,
-  
+  list: {
+    Wife: 5053770520,
+    'Father': 9050803377,
+    'Mother': 9050801133,
+    'Brother': 9050805599,
+    'Auto Service': 9132338866,
+    Barnaul: 666666,
+  }, 
+
   add(name, number) {
-    this[name] = number;
+    this.list[name] = number;
   },
   
   del(name) {
-    delete this[name];
+    delete this.list[name];
   },
 }
+//add new contact
+telegramContacts.list.Airport = 3852337755;
 
-telegramContacts.Friend = 9134552244;
-telegramContacts['Friend 1'] = 9137666600;
-telegramContacts.Friend = 'not a number';
+//add new contact #2
+telegramContacts.list['Bus station'] = 3852357593;
 
-delete telegramContacts['Friend 0'];
-delete telegramContacts.Barnaul;
+// delete contact
+delete telegramContacts.list.Barnaul;
 
+
+// add new contact using method 'add'
 telegramContacts.add('Evgenia', 79050803377);
 telegramContacts.add('Unknown', 79502335911);
 
+// delete contact using method 'del'
 telegramContacts.del('Unknown');
-telegramContacts.del('Friend 1');
 
-console.log(telegramContacts);
+
+// вывод всем свойств (ключей и значений) объекта «list»
+for(const key in telegramContacts.list) {
+  console.log(`${key} - ${telegramContacts.list[key]}`);
+}
