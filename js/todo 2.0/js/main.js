962246b1bb67d3b53ee74c0a20666b0367c70584
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-03-07 22:03:06 +0300

    add todo 2.0

---
+++ js/todo 2.0/js/main.js
@@ -0,0 +1,83 @@
+const STATUS = {
+  TODO: 'Todo',
+  DONE: 'Done',
+}
+      
+const PRIORITY = {
+  LOW: 'Low',
+  HIGH: 'High',
+}
+
+const highForm = document.querySelector('.high_task_form'),
+      lowForm = document.querySelector('.low_task_form'),
+      highInput = document.querySelector('.high_input'),
+      lowInput = document.querySelector('.low_input'),
+      highTaskList = document.querySelector('.high_task_list'),
+      lowTaskList = document.querySelector('.low_task_list');
+
+let tasks = [];
+
+function addTask(event) {
+  event.preventDefault();
+
+  let taskText;
+  let priority;
+
+  const highTaskHTML = `<div class="task">
+  <input class="radio" type="radio">
+  <p class="task_text">${highInput.value}</p>
+  <button class="delete_button" type="submit"></button>
+</div>`;
+
+  const lowTaskHTML = `<div class="task">
+  <input class="radio" type="radio">
+  <p class="task_text">${lowInput.value}</p>
+  <button class="delete_button" type="submit"></button>
+  </div>`;
+
+
+  if(event.target.classList.value == 'high_task_form') {
+    highTaskList.insertAdjacentHTML('beforeend', highTaskHTML);
+    taskText = highInput.value;
+    priority = PRIORITY.HIGH;
+  } else {
+    lowTaskList.insertAdjacentHTML('beforeend', lowTaskHTML);
+    taskText = lowInput.value;
+    priority = PRIORITY.LOW;
+  }
+
+  const newTask = {
+    id: Date.now(),
+    name: taskText,
+    status: STATUS.TODO,
+    priority: priority,
+  }
+  
+  tasks.push(newTask);
+  
+  highInput.value = '';
+  lowInput.value = '';
+
+  console.log(tasks);
+}
+
+function deleteTask(event) {
+  if(event.target.className === 'delete_button') {
+    const parentNode = event.target.closest('.task');
+    const childNode = event.target.previousElementSibling;
+    const taskContent = childNode.textContent;
+
+    const index = tasks.findIndex(task => task.name == taskContent);
+
+    tasks.splice(index, 1);
+    console.log(tasks);
+
+    parentNode.remove();
+  }
+}
+
+highForm.addEventListener('submit', addTask);
+lowForm.addEventListener('submit', addTask);
+
+highTaskList.addEventListener('click', deleteTask);
+lowTaskList.addEventListener('click', deleteTask);
