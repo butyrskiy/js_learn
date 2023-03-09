641f9eaa34edc7dce06a07133ad0e9042d0e5fdd
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-03-09 15:19:52 +0300

    add function render and fix function "checkTask"

--- js/todo 2.0/js/main.js
+++ js/todo 2.0/js/main.js
@@ -28,31 +28,15 @@ let tasks = [];
 
 function addTask(event) {
   event.preventDefault();
-
-  let taskText;
   let priority;
+  let taskText;
 
-  const highTaskHTML = `<div class="task">
-  <input class="radio" type="radio">
-  <p class="task_text">${highInput.value}</p>
-  <button class="delete_button" type="submit"></button>
-</div>`;
-
-  const lowTaskHTML = `<div class="task">
-  <input class="radio" type="radio">
-  <p class="task_text">${lowInput.value}</p>
-  <button class="delete_button" type="submit"></button>
-  </div>`;
-
-
-  if(event.target.classList.value == 'high_task_form') {
-    highTaskList.insertAdjacentHTML('beforeend', highTaskHTML);
-    taskText = highInput.value;
+  if(event.target.className === 'high_task_form') {
     priority = PRIORITY.HIGH;
+    taskText = highInput.value;
   } else {
-    lowTaskList.insertAdjacentHTML('beforeend', lowTaskHTML);
-    taskText = lowInput.value;
     priority = PRIORITY.LOW;
+    taskText = lowInput.value;
   }
 
   const newTask = {
@@ -63,6 +47,7 @@ function addTask(event) {
   }
   
   tasks.push(newTask);
+  render(newTask);
   
   highInput.value = '';
   lowInput.value = '';
@@ -73,21 +58,57 @@ function addTask(event) {
 function deleteTask(event) {
   if(event.target.className === 'delete_button') {
     const parentNode = event.target.closest('.task');
-    const childNode = event.target.previousElementSibling;
-    const taskContent = childNode.textContent;
-
-    const index = tasks.findIndex(task => task.name == taskContent);
+    const previousElement = event.target.previousElementSibling;
+    const taskContent = previousElement.textContent;
+    
+    const index = searchTaskIndex(taskContent);
 
     tasks.splice(index, 1);
-    console.log(tasks);
-
     parentNode.remove();
+
+    console.log(tasks);
   }
 }
 
 function checkTask(event) {
   const parentNode = event.target.parentNode;
+  const childNode = parentNode.children[1];
+  const taskContent = childNode.textContent;
+  
+  const index = searchTaskIndex(taskContent);  
+
   if(event.target.className === 'radio') {
     parentNode.classList.toggle('done');
+    tasks[index].status = STATUS.DONE;
+  }
+
+  console.log(tasks);
+}
+
+function searchTaskIndex(taskContent) {
+  const index = tasks.findIndex(task => task.name === taskContent);
+
+    return index;
+}
+
+function render(task) {
+
+  const highTaskHTML = `<div class="task">
+  <input class="radio" type="radio">
+  <p class="task_text">${task.name}</p>
+  <button class="delete_button" type="submit"></button>
+</div>`;
+
+  const lowTaskHTML = `<div class="task">
+  <input class="radio" type="radio">
+  <p class="task_text">${task.name}</p>
+  <button class="delete_button" type="submit"></button>
+  </div>`;
+
+
+  if(task.priority == PRIORITY.HIGH) {
+    highTaskList.insertAdjacentHTML('beforeend', highTaskHTML);
+  } else {
+    lowTaskList.insertAdjacentHTML('beforeend', lowTaskHTML);
   }
 }
