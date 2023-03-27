d5e1f61e585ad8c19cbacb4e1de9d4f208a40c74
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-03-27 22:05:12 +0300

    added modules

--- task_strada/st21_55_weather/main.js
+++ task_strada/st21_55_weather/main.js
@@ -1,25 +1,4 @@
-const searchForm = document.querySelector('.search-form'),
-      searchInput = document.querySelector('.search-input'),
-      temperatureNow = document.querySelector('.temperature'),
-      cityNow = document.querySelector('.info-current-city'),
-      addCity = document.querySelector('.add-city'),
-      locationsList = document.querySelector('.locations-list'),
-      tabsList = document.querySelector('.tabs-list'),
-      tabsNow = document.querySelector('.tabs-now'),
-      tabsDetails = document.querySelector('.tabs-details'),
-      tabsForecast = document.querySelector('.tabs-forecast'),
-      detailsCurrentCity = document.querySelector('.details-current-city'),
-      detailsTemperature = document.querySelector('.temperature-details-content'),
-      detailsFeelsLike = document.querySelector('.feels_like-details-content'),
-      detailsWeather = document.querySelector('.weather-details-content'),
-      detailsSunrise = document.querySelector('.sunrise-details-content'),
-      detailsSunset = document.querySelector('.sunset-details-content');
-
-      
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const cityName = 'boston';
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+import {searchForm, searchInput, temperatureNow, cityNow, addCity, locationsList, tabsList, tabsNow, tabsDetails, tabsForecast, detailsCurrentCity, detailsTemperature, detailsFeelsLike, detailsWeather, detailsSunrise, detailsSunset, serverUrl, cityName, apiKey, url} from './modules/constants.js'
 
 const cityArr = [];
 let locationsArr = [];
