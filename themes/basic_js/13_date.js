4615ac0e3c6dc88db0fcd75492720e9ea3deabfd
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-04-06 22:19:34 +0300

    added new theme in basic js - "date"

--- themes/basic_js/13_date.js
+++ themes/basic_js/13_date.js
@@ -1,57 +1,74 @@
-// ! Встроенный объект: Date. Он содержит дату и время, а также предоставляет методы управления ими
+// ! ВСТРОЕННЫЙ ОБЪЕКТ: Date. Он содержит дату и время, а также предоставляет методы управления ими
 
 // Todo. Для создания нового объекта Date нужно вызвать конструктор new Date()
-
 // ? Вызов без аргументов вернёт текущую дату и время
 let now = new Date();
 // console.log(now); // ? Tue Apr 04 2023 16:25:58 GMT+0300 (GMT+03:00)
 
 
-// Todo. Создание объекта даты. Обязательны первые два параметра
-// ? year должен состоять из четырёх цифр. Для совместимости также принимаются 2 цифры и рассматриваются как 19xx, к примеру, 98 здесь это тоже самое, что и 1998, но настоятельно рекомендуется всегда использовать 4 цифры.
-// ? month начинается с 0 (январь) по 11 (декабрь).
-// ? Параметр date здесь представляет собой день месяца. Если параметр не задан, то принимается значение 1.
-// ? Если параметры hours/minutes/seconds/ms отсутствуют, их значением становится 0.
+// ? Передаём данные
+now = new Date('2020-05-12');
+// console.log(now); // ? Tue May 12 2020 03:00:00 GMT+0300 (GMT+03:00)
 
-// new Date(year, month, date, hours, minutes, seconds, ms)
-
-
- // Todo. ПОЛУЧЕНИЕ КОМПОНЕНТОВ ДАТЫ
- // ? Существуют методы получения года, месяца и т.д. из объекта Date:
-
-// getFullYear() // ? Получить год (4 цифры)
-// getMonth() // ? Получить месяц, от 0 до 11.
-// getDay() // ? Получить определённый день недели:
-// getDate() // ? Получить день месяца, от 1 до 31, что несколько противоречит названию метода.
-// getHours(), getMinutes(), getSeconds(), getMilliseconds() // ? Получить, соответственно, часы, минуты, секунды или миллисекунды.
-
-// console.log(now.getFullYear()); // 2023
-// console.log(now.getMonth()); // 3-апрель, так как 0-январь
-// console.log(now.getDay()); // 0-воскресенье, 1-пн, 2-вт
-// console.log(now.getDate()); // 4
-// console.log(now.getHours()); // 16
-// console.log(now.getHours()); // 16
-// console.log(now.getMinutes()); // 53
-// console.log(now.getSeconds()); // 10
-// console.log(now.getMilliseconds()); // 729
+now = new Date(2020, 4, 12, 22);
+// console.log(now); // ? Sat May 12 2020 22:00:00 GMT+0300 (GMT+03:00) (январь - 0)
 
 
-// Todo. МЕТОД «getTime» возвращает таймстамп – количество миллисекунд, прошедших с 1 января 1970 года UTC+0.
-console.log(now.getTime()); // 1680616455294
+// Todo. Timestamp - время в миллисекундах с начала 1970 года
+// ? Все даты, которые мы создаём это даты в миллисекундах, которые преобразуются в дату и дату также можно преобразовать в миллисекунды
+
+now = new Date(0);
+// console.log(now); // ? Thu Jan 01 1970 02:00:00 GMT+0200 (GMT+03:00)
+
+// ? Чтобы получить дату до 1970 года используем отрицательные значения
+now = new Date(-9999999999999);
+// console.log(now); // ? Mon Feb 10 1653 08:09:12 GMT+0155 (GMT+03:00)
 
 
-// Todo. УСТАНОВКА КОМПОНЕНТОВ ДАТЫ
-// ? Следующие методы позволяют установить компоненты даты и времени:
-// ? У всех этих методов, кроме setTime(), есть UTC-вариант, например: setUTCHours().
 
-// setFullYear(year, [month], [date])
-// setMonth(month, [date])
-// setDate(date)
-// setHours(hour, [min], [sec], [ms])
-// setMinutes(min, [sec], [ms])
-// setSeconds(sec, [ms])
-// setMilliseconds(ms)
-// setTime(milliseconds) (устанавливает дату в виде целого количества миллисекунд, прошедших с 01.01.1970 UTC)
+// ! МЕТОДЫ ДЛЯ РАБОТЫ С ДАТОЙ
 
-now.setFullYear(2030);
-// console.log(now); // ? Thu Apr 04 2030 16:58:22 GMT+0300 (GMT+03:00)
+// Todo. ПОЛУЧЕНИЕ КОМПОНЕНТОВ ДАТЫ (геттеры)
+
+now = new Date();
+// console.log(now.getFullYear()); // ? 2023 - год
+// console.log(now.getMonth()); // ? 3 - апрель
+// console.log(now.getDay()); // ? 4 - день недели (четверг)
+// console.log(now.getHours()); // ? 21
+// console.log(now.getMinutes()); // ? 48
+// console.log(now.getSeconds()); // ? 47
+// console.log(now.getMilliseconds()); // ? 542
+// console.log(now.getDate()); // ? 6 - число месяца
+
+// console.log(now.getTimezoneOffset()); // ? -180 - разница в минутах между текущим часовым поясом и UTC
+// console.log(now.getTime()); // ? 1680806968864 - количество миллисекунд прошедшее с 1 января 1970 года
+
+// Todo. Превратить миллисекунды обратно в дату
+now = new Date(1680806968864)
+// console.log(now) // ? Thu Apr 06 2023 21:49:28 GMT+0300 (GMT+03:00)
+
+
+// Todo. УСТАНОВКА КОМПОНЕНТОВ ДАТЫ (сеттеры)
+
+now = new Date();
+// console.log(now.setHours(18)); // ? 1680793557253
+// console.log(now); // ? Thu Apr 06 2023 18:05:57 GMT+0300 (GMT+03:00)
+// console.log(now.setFullYear());
+// console.log(now.setMonth());
+// console.log(now.setDay());
+// console.log(now.setMinutes());
+// console.log(now.setSeconds());
+// console.log(now.setMilliseconds());
+
+
+// Todo. ПОСЧИТАТЬ РАЗНИЦУ ВО ВРЕМЕНИ МЕЖДУ ДАТАМИ
+
+let start = new Date();
+
+for(let i = 0; i < 100000; i++) {
+  let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
